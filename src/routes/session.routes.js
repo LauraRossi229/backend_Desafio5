d03984aaef294d66d62176ea770421d8519c1c44
Laura Rossi <rossil229@gmail.com>
2023-09-23 18:22:52 -0300

    segundo commit

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -5,7 +5,7 @@ const sessionRouter = Router()
 
 // Ruta para mostrar el formulario de inicio de sesión
 sessionRouter.get('/login', (req, res) => {
-    res.render('login'); // Renderiza la vista de inicio de sesión
+    res.render('login', { successMessage: 'Loguearse' }); // Renderiza la vista de inicio de sesión
 });
 
 sessionRouter.post('/login', async (req, res) => {
@@ -13,7 +13,8 @@ sessionRouter.post('/login', async (req, res) => {
 
     try {
         if (req.session.login) {
-            res.status(200).send({ resultado: 'Login ya existente' })
+            return res.status(200).render('login', { errorMessage: 'Usuario Logueado, cierre sesión y vuelva a ingresar' });
+  
         }
         const user = await userModel.findOne({ email: email })
 
@@ -26,19 +27,19 @@ sessionRouter.post('/login', async (req, res) => {
                     email: user.email
                 };
                 //res.render('products', { userData }); // Redirecciona al usuario a la vista de productos
-                res.redirect(`/products?first_name=${userData.first_name}&last_name=${userData.last_name}&email=${userData.email}`);
+              return  res.redirect(`/products?first_name=${userData.first_name}&last_name=${userData.last_name}&email=${userData.email}`);
             } else {
                 //res.status(401).send({ resultado: 'Contaseña no valida', message: password })
-                res.status(401).render('login', { errorMessage: 'Contraseña no válida' });
+               return res.status(401).render('login', { errorMessage: 'Contraseña no válida' });
             }
         } else {
             //res.status(404).send({ resultado: 'Not Found', message: user })
-            res.status(404).render('login', { errorMessage: 'Usuario no encontrado' });
+            return res.status(404).render('login', { errorMessage: 'Usuario no encontrado' });
         }
 
     } catch (error) {
         //res.status(400).send({ error: `Error en Login: ${error}` })
-        res.status(400).render('login', { errorMessage: `Error en el inicio de sesión: ${error}` });
+        return res.status(400).render('login', { errorMessage: `Error en el inicio de sesión: ${error}` });
     }
 })
 
